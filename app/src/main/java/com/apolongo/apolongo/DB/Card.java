af6d295514f2f6867de50a940a02d74b4a17c30e
Ángel <angel.angel802@gmail.com>
2019-01-22 19:44:57 +0100

    Create Card.java

---
+++ app/src/main/java/com/apolongo/apolongo/DB/Card.java
@@ -0,0 +1,40 @@
+package com.apolongo.apolongo.DB;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "card_table")
+public class Card {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "card_name")
+    private String mCardName;
+    @NonNull
+    @ColumnInfo(name = "card_image")
+    private String mImageName;
+    @NonNull
+    @ColumnInfo(name = "card_cycle")
+    private int mBillingCycle;
+
+    public Card(@NonNull String card, @NonNull String image, @NonNull int cycle){
+        this.mCardName = card;
+        this.mImageName = image;
+        this.mBillingCycle = cycle;
+    }
+
+    public String getCardName(){return mCardName;}
+    public String getImageName(){return mImageName;}
+    public int getBillingCycle(){return mBillingCycle;}
+
+    public void setCardName(String cardName) {
+        this.mCardName = cardName;
+    }
+    public void setImage(String imageName) {
+        this.mImageName = imageName;
+    }
+    public void setBillingCycle(int cycle) {
+        this.mBillingCycle = cycle;
+    }
+}
