5b826ff0a040c74bcf8ec785a198af625a91224a
IronSenior <pepemarquezof@gmai.com>
2019-01-22 20:30:40 +0100

    PurchaseDao creado

---
+++ app/src/main/java/com/apolongo/apolongo/DB/PurchaseDao.java
@@ -0,0 +1,25 @@
+package com.apolongo.apolongo.DB;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PurchaseDao {
+    @Insert
+    void insert(Purchase purchase);
+
+    @Delete
+    void delete(Purchase purchase);
+
+    @Query("DELETE FROM purchase_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM purchase_table ORDER BY purchase_date DESC")
+    LiveData<List<Purchase>> getAllPurchases();
+
+}
