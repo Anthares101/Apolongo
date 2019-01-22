ed079de1274ed4f132dabd96005a10054fef0b18
Ángel <angel.angel802@gmail.com>
2019-01-22 20:36:34 +0100

    Update ApolongoDB.java

--- app/src/main/java/com/apolongo/apolongo/DB/ApolongoDB.java
+++ app/src/main/java/com/apolongo/apolongo/DB/ApolongoDB.java
@@ -1,10 +1,28 @@
 package com.apolongo.apolongo.DB;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {Card.class, Purchase.class}, version = 1)
 public abstract class ApolongoDB extends RoomDatabase {
     public abstract CardDao cardDao();
-    public abstract PurchaDao purchaseDao();
+    public abstract PurchaseDao purchaseDao();
+
+    private static volatile ApolongoDB INSTANCE;
+
+    static ApolongoDB getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (ApolongoDB.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ApolongoDB.class, "apolongo_database")
+                            .fallbackToDestructiveMigration()//Destroy the DB if the schema changes
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
