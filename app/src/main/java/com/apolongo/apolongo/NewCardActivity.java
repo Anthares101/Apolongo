50d855f7f34da6e07bd5d3c46e0d1909197aa827
José Márquez Doblas <pepemarquezof@gmail.com>
2019-01-23 18:12:35 +0100

    some chages

--- app/src/main/java/com/apolongo/apolongo/NewCardActivity.java
+++ app/src/main/java/com/apolongo/apolongo/NewCardActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 
 public class NewCardActivity extends AppCompatActivity {
 
-    public static final String EXTRA_REPLY = "com.apolongo.android.wordlistsql.REPLY";
+    public static final String EXTRA_REPLY = "com.apolongo.android.cardlistsql.REPLY";
 
     private EditText mEditCardView;
 
