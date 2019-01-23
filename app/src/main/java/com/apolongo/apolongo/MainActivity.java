d49eac75ac20bd81e9c0a598e5d0f23286a16c0f
José Márquez Doblas <pepemarquezof@gmail.com>
2019-01-23 18:06:39 +0100

    + Button funcionality added

--- app/src/main/java/com/apolongo/apolongo/MainActivity.java
+++ app/src/main/java/com/apolongo/apolongo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.apolongo.apolongo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent NewCard = new Intent(getApplicationContext(), NewCardActivity.class);
+                startActivity(NewCard);
             }
         });
 
