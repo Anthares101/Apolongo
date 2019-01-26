4ff964aa46358d5fff0e322bd7557083841fc077
Ángel <angel.angel802@gmail.com>
2019-01-26 05:06:34 +0100

    Purchases for a certain card are now sort by date

--- app/src/main/java/com/apolongo/apolongo/DB/PurchaseDao.java
+++ app/src/main/java/com/apolongo/apolongo/DB/PurchaseDao.java
@@ -22,7 +22,7 @@ public interface PurchaseDao {
     @Query("SELECT * FROM purchases_table ORDER BY purchase_date DESC")
     LiveData<List<Purchase>> getAllPurchases();
 
-    @Query("SELECT * FROM purchases_table WHERE purchase_CardName = :cardName")
+    @Query("SELECT * FROM purchases_table WHERE purchase_CardName = :cardName ORDER BY purchase_date DESC")
     LiveData<List<Purchase>> getPurchase(String cardName);
 
 }
